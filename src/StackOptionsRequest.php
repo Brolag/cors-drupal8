bcb3bf00430c15f880cd1e3b09eb13b2bc8b45cb
Alfredo Bonilla <josebonillav@gmail.com>
2015-02-03 21:08:57 -0600

    Make the code looks pretty

--- src/StackOptionsRequest.php
+++ src/StackOptionsRequest.php
@@ -38,29 +38,27 @@ class StackOptionsRequest implements HttpKernelInterface {
    * {@inheritDoc}
    */
     public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = TRUE) {
-    $response = $this->app->handle($request, $type, $catch);
+      $response = $this->app->handle($request, $type, $catch);
 
-     // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
+      // Allow from any origin
+      if (isset($_SERVER['HTTP_ORIGIN'])) {
         $response->headers->set('Access-Control-Allow-Origin', "{$_SERVER['HTTP_ORIGIN']}");
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
         $response->headers->set('Access-Control-Max-Age', '86400');
-    }
+      }
 
-    // Allow headers for OPTIONS method [estas son mis lineas]
-    if ($request->getMethod() == 'OPTIONS') {
+      // Allow headers for OPTIONS method
+      if ($request->getMethod() == 'OPTIONS') {
         $response->setContent(Null);
         $response->setStatusCode(200);
-   
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');         
         }
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
           $response->headers->set('Access-Control-Allow-Headers', "{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");      
         }
-    }
-    // Aqui terminan mis lineas
+      }
 
-    return $response;
-  }
+      return $response;
+    }
 }
